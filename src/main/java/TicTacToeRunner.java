8ad2533ebb29470b8b80b78e88f86c8f377a4b96
Matt Glover <matthewglover@me.com>
2017-07-20 13:18:14 +0100

    Adds GameStatus to TicTacToeRunner

--- src/main/java/TicTacToeRunner.java
+++ src/main/java/TicTacToeRunner.java
@@ -1,27 +1,56 @@
-public class TicTacToeRunner {
+import java.util.Observable;
+import java.util.Observer;
+
+public class TicTacToeRunner implements Observer {
     private final GameOptionsUI gameOptionsUI;
     private final PlayerUI playerUI;
+    private GameStatusUI gameStatusUI;
     private GameOptions gameOptions;
     private Game game;
     private Player playerX;
     private Player playerO;
+    private boolean active = true;
+    private GameStatus gameStatus;
 
     public static void main(String[] args) {
         GameOptionsUI gameOptionsUI = new GameOptionsUI(System.in, System.out);
         PlayerUI playerUI = new PlayerUI(System.in, System.out);
-        TicTacToeRunner ticTacToeRunner = new TicTacToeRunner(gameOptionsUI, playerUI);
+        GameStatusUI gameStatusUI = new GameStatusUI(System.in, System.out);
+        TicTacToeRunner ticTacToeRunner = new TicTacToeRunner(gameOptionsUI, playerUI, gameStatusUI);
         ticTacToeRunner.execute();
     }
 
-    public TicTacToeRunner(GameOptionsUI gameOptionsUI, PlayerUI playerUI) {
+    public TicTacToeRunner(GameOptionsUI gameOptionsUI, PlayerUI playerUI, GameStatusUI gameStatusUI) {
         this.gameOptionsUI = gameOptionsUI;
         this.playerUI = playerUI;
+        this.gameStatusUI = gameStatusUI;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        active = (Boolean) arg;
+
+        if (active) {
+            execute();
+        }
+    }
+
+    public PlayerSymbol getWinner() {
+        return game.getWinner();
     }
 
     public void execute() {
         buildGameOptions();
+        buildGameStatus();
+        buildPlayers();
         buildGame();
+        registerGameObservers();
+        observeGameStatus();
         startGame();
+    }
+
+    public boolean isActive() {
+        return active;
     }
 
     private void buildGameOptions() {
@@ -29,19 +58,30 @@ public class TicTacToeRunner {
         gameOptions.execute();
     }
 
-    private void buildGame() {
-        game = new Game(gameOptions.getBoardSize());
+    private void buildGameStatus() {
+        gameStatus = new GameStatus(gameStatusUI);
+    }
+
+    private void buildPlayers() {
         playerX = new Player(playerUI, PlayerSymbol.X);
         playerO = new Player(playerUI, PlayerSymbol.O);
+    }
+
+    private void buildGame() {
+        game = new Game(gameOptions.getBoardSize());
+    }
+
+    private void registerGameObservers() {
         game.addObserver(playerX);
         game.addObserver(playerO);
+        game.addObserver(gameStatus);
+    }
+
+    private void observeGameStatus() {
+        gameStatus.addObserver(this);
     }
 
     private void startGame() {
         game.start();
-    }
-
-    public PlayerSymbol getWinner() {
-        return game.getWinner();
     }
 }
--- src/test/java/TicTacToeRunnerTest.java
+++ src/test/java/TicTacToeRunnerTest.java
@@ -8,10 +8,26 @@ public class TicTacToeRunnerTest {
     public void configuresAndExecutesGame() {
         GameOptionsUIBuilder optionsBuilder = new GameOptionsUIBuilder("3\n1\n");
         PlayerUIBuilder playerBuilder = new PlayerUIBuilder("1\n4\n2\n5\n3\n");
+        GameStatusUIBuilder gameStatusBuilder = new GameStatusUIBuilder("n\n");
         TicTacToeRunner runner = new TicTacToeRunner(
                 optionsBuilder.getGameOptionsUI(),
-                playerBuilder.getPlayerUI());
+                playerBuilder.getPlayerUI(),
+                gameStatusBuilder.getGameStatusUI());
         runner.execute();
         assertEquals(PlayerSymbol.X, runner.getWinner());
+        assertFalse(runner.isActive());
+    }
+
+    @Test
+    public void startsNewGameOnUserInput() {
+        GameOptionsUIBuilder optionsBuilder = new GameOptionsUIBuilder("3\n1\n");
+        PlayerUIBuilder playerBuilder = new PlayerUIBuilder("1\n4\n2\n5\n3\n");
+        GameStatusUIBuilder gameStatusBuilder = new GameStatusUIBuilder("y\n");
+        TicTacToeRunner runner = new TicTacToeRunner(
+                optionsBuilder.getGameOptionsUI(),
+                playerBuilder.getPlayerUI(),
+                gameStatusBuilder.getGameStatusUI());
+        runner.execute();
+        assertTrue(runner.isActive());
     }
 }
