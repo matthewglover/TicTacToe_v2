456b59ba685c22b7ce52b171cb05293f1deb4c0e
Matt Glover <matthewglover@me.com>
2017-07-31 15:48:54 +0100

    Cleans up SceneSelectorTest

--- src/test/java/com/matthewglover/tictactoe/gui/SceneSelectorTest.java
+++ src/test/java/com/matthewglover/tictactoe/gui/SceneSelectorTest.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.util.WaitForAsyncUtils;
 
 import java.util.concurrent.FutureTask;
 import java.util.function.Supplier;
@@ -19,12 +18,13 @@ import static org.testfx.matcher.base.NodeMatchers.hasChildren;
 
 public class SceneSelectorTest extends ApplicationTest {
 
+    private final int GAME_STATUS_DELAY = 0;
     private final Model model = new Model();
     private SceneSelector sceneSelector;
 
     @Override
     public void start(Stage stage) throws Exception {
-        sceneSelector = new SceneSelector(model, 0);
+        sceneSelector = new SceneSelector(model, GAME_STATUS_DELAY);
         stage.setScene(sceneSelector.getScene());
         stage.show();
         stage.toFront();
@@ -32,49 +32,71 @@ public class SceneSelectorTest extends ApplicationTest {
 
     @Test
     public void initialSceneIsGameOptions() {
-        Parent rootNode = sceneSelector.getScene().getRoot();
-        verifyThat(rootNode, hasChildren(GameType.values().length, ".button"));
-        for (int i = 0; i < GameType.values().length; i++) {
-            Button currentButton = from(rootNode).lookup(".button").nth(i).query();
-            assertEquals(GameType.values()[i].getDescription(), currentButton.getText());
-        }
+        verifyGameOptionsScene();
     }
 
     @Test
     public void sceneIsBoardAfterGameTypeSelected() {
         model.setGameType(GameType.HUMAN_HUMAN);
-        Parent rootNode = sceneSelector.getScene().getRoot();
-        verifyThat(rootNode, hasChildren(9, ".button"));
+        verifyBoardScene();
     }
 
     @Test
     public void sceneIsGameStatusAfterGameOver() throws Exception {
-        FutureTask<Void> query = new FutureTask<>(() -> {
-            model.setGameType(GameType.HUMAN_HUMAN);
-            model.move(1);
-            model.move(4);
-            model.move(2);
-            model.move(5);
-            model.move(3);
-            return null;
-        });
+        FutureTask<Void> query = runWinningGameForX();
 
         Platform.runLater(query);
         query.get();
 
+        verifyGameStatusScene();
+    }
+
+    @Test
+    public void sceneIsGameOptionsAfterClickNewGame() throws Exception {
+        FutureTask<Void> query = runWinningGameForX();
+
+        Platform.runLater(query);
+        query.get();
+
+        runDelayed(50, () -> {
+            clickOn("#new_game");
+            return sceneSelector.getScene().getRoot();
+        });
+
+        verifyGameOptionsScene();
+    }
+
+    private void verifyGameOptionsScene() {
+        verifyThat(getRootNode(), hasChildren(GameType.values().length, ".button"));
+        for (int i = 0; i < GameType.values().length; i++) {
+            Button currentButton = from(getRootNode()).lookup(".button").nth(i).query();
+            assertEquals(GameType.values()[i].getDescription(), currentButton.getText());
+        }
+    }
+
+    private void verifyBoardScene() {
+        verifyThat(getRootNode(), hasChildren(9, ".button"));
+    }
+
+    private void verifyGameStatusScene() {
         String text = runDelayed(50, () -> {
-            Parent rootNode = sceneSelector.getScene().getRoot();
-            Label label = from(rootNode).lookup("#game_result").query();
+            Label label = from(getRootNode()).lookup("#game_result").query();
             return label.getText();
         });
 
         assertEquals("X wins!", text);
     }
 
-    @Test
-    public void sceneIsGameOptionsAfterClickNewGame() throws Exception {
-        FutureTask<Void> query = new FutureTask<>(() -> {
+    private Parent getRootNode() {
+        return sceneSelector.getScene().getRoot();
+    }
+
+    private FutureTask<Void> runWinningGameForX() {
+        return new FutureTask<>(() -> {
             model.setGameType(GameType.HUMAN_HUMAN);
+            // x x x
+            // o o 6
+            // 7 8 9
             model.move(1);
             model.move(4);
             model.move(2);
@@ -82,16 +104,6 @@ public class SceneSelectorTest extends ApplicationTest {
             model.move(3);
             return null;
         });
-
-        Platform.runLater(query);
-        query.get();
-
-        Parent rootNode = runDelayed(50, () -> {
-            clickOn("#new_game");
-            return sceneSelector.getScene().getRoot();
-        });
-
-        verifyThat(rootNode, hasChildren(GameType.values().length, ".button"));
     }
 
     private <T extends Object> T runDelayed(int milliSecondsDelay, Supplier<T> lambda) {
