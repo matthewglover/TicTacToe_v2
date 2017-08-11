ca7b6939ede13731ac5ab098178b4aeb93719de0
Matt Glover <matthewglover@me.com>
2017-08-11 12:05:56 +0100

    Adds annotation to DelayedRunner

--- src/main/java/com/matthewglover/tictactoe/core/DelayedRunner.java
+++ src/main/java/com/matthewglover/tictactoe/core/DelayedRunner.java
@@ -3,6 +3,7 @@ package com.matthewglover.tictactoe.core;
 import java.util.concurrent.FutureTask;
 
 public class DelayedRunner implements Runner {
+    @Override
     public void run(int milliSecondsDelay, Runnable runnable) {
         FutureTask<Void> sleeper = new FutureTask<>(() -> {
             Thread.sleep(milliSecondsDelay);
